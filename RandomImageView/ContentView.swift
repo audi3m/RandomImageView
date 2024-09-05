a8a001528f868697b192fa13820b109ee46f8ff2
ray <audi3m@naver.com>
2024-09-05 20:33:05 +0900

    Initial Commit

---
+++ RandomImageView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomImageView
+//
+//  Created by J Oh on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomImageView/RandomImageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageViewApp.swift
+//  RandomImageView
+//
+//  Created by J Oh on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
